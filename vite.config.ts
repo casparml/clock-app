e9caf1c10cf10eed82bc874b717c11d2926dde1f
Caspar Landman <info@casparlandman.nl>
2025-11-01 23:03:05 +0100

    Update `vite.config.ts` to simplify `base` URL to root path

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ const __dirname = fileURLToPath(new URL('.', import.meta.url));
 
 export default defineConfig({
     plugins: [react()],
-    base: '/clock-app/',
+    base: '/',
     root: './',
     publicDir: 'public',
     build: {
